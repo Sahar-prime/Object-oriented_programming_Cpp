8a763bc1d739686a109cbb367cc7ee796f90180a
user <user@WIN-4KGTNI4DDSK>
2026-01-28 17:24:44 +0300

    Add project files.

--- Object-oriented_programming_C++/h_w_7.cpp
+++ Object-oriented_programming_C++/h_w_7.cpp
@@ -2,9 +2,61 @@
 
 //#define H_W_7
 
-#ifdef H_W_7
-int main() 
+class Employer
 {
+public:
+    virtual void Print() const 
+    {
+        std::cout << "Employer: Базовый служащий.\n";
+    }
+};
+class President : public Employer
+{
+public:
+    void Print() const
+    {
+        std::cout << "Президент: Руководит компанией, принимает стратегические решения.\n";
+    }
+};
+class Manager : public Employer
+{
+public:
+    void Print() const
+    {
+        std::cout << "Менеджер: Управляет отделом, координирует работу сотрудников.\n";
+    }
+};
+class Worker : public Employer
+{
+public:
+    void Print() const
+    {
+        std::cout << "Рабочий: Выполняет основные производственные задачи.\n";
+    }
+};
 
+#ifdef H_W_7
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Employer* employees[] =
+    {
+        new President(),
+        new Manager(),
+        new Worker()
+    };
+    int user;
+    std::cout << "Выбери служащего:\n1 - Президент\n2 - Менеджер\n3 - Рабочий\n";
+    std::cout << "Ваш выбор: ";
+    std::cin >> user;
+    if (user >= 4 or user <= 0)
+    {
+        std::cout << "Такого рабочего нет!";
+    }
+    else
+    {
+        employees[user - 1]->Print();
+    }
 }
 #endif //H_W_7
